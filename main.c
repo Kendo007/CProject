8b23dfa2b28a0937cb5208ab2366f2a2c4219306
Kheyanshu <110466301+Kendo007@users.noreply.github.com>
2024-09-06 11:40:46 +0530

    added

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "helper/hashmap.h"
-#include "helper/myhashmap.h"
-#include "src/HackAssembler/AssemblerSecondPass.h"
-#include "src/HackAssembler/HackAssembler.h"
-#include "tests/Test.h"
 
-int main() {
-    HackAssembler("test.txt");
+#include <stdio.h>
+#include <time.h>
+
+#include "src/HackAssembler/HackAssembler.h"
+
+int main(int argc, char * argv[]) {
+    clock_t start = clock();
+
+    HackAssembler(argv[1]);
+
+    clock_t stop = clock();
+    double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
+    printf("\nTime elapsed: %.2f milliseconds", elapsed * 1000);
+
+    return 0;
 }
 
