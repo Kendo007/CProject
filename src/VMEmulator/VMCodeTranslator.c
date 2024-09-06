4b7a6e6a3046f9992ca231b2912ebb389e6e75fb
Kheyanshu <110466301+Kendo007@users.noreply.github.com>
2024-09-06 20:34:04 +0530

    Completed Function Command Implementation

--- src/VMEmulator/VMCodeTranslator.c
+++ src/VMEmulator/VMCodeTranslator.c
@@ -5,6 +5,7 @@
 #include "VMCodeTranslator.h"
 
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../../helper/hashmap.h"
@@ -138,7 +139,7 @@ void booleanPush(const char* jumpCondition) {
     write("A=M-1");
     write("M=0");
 
-    char temp[11]
+    char temp[11];
     snprintf(temp, 11, "(CONT%d)", boolCount);
     write(temp);
 
@@ -171,9 +172,9 @@ bool arthlogiCommand(const char* Command[]) {
             write("M=D|M");
         else if (strcmp(Command[0], "eq") == 0)
             booleanPush("JEQ");
-        else if (strcmp(Command[0], ) == 0)
+        else if (strcmp(Command[0], "gt") == 0)
             booleanPush("JGT");
-        else if (strcmp(Command[0], ) == 0)
+        else if (strcmp(Command[0], "lt") == 0)
             booleanPush("JLT");
         else
             return false;
@@ -213,6 +214,175 @@ bool branchingCommand(const char* Command[]) {
     return true;
 }
 
+void helperCall(const char* addr) {
+    write(addr);
+    if (strchr(addr, '.') != NULL) {
+        write("D=A");
+    } else {
+        write("D=M");
+    }
+    write("@SP");
+    write("M=M+1");
+    write("A=M-1");
+    write("M=D");
+}
+
+void commonCall() {
+    write("(FUNCTION)");
+    helperCall("@LCL");
+    helperCall("@ARG");
+    helperCall("@THIS");
+    helperCall("@THAT");
+
+    // Setting Up New ARG
+    write("@R13");
+    write("D=M");
+    write("@ARG");
+    write("M=D");
+
+    // Setting Up new LCL
+    write("@SP");
+    write("D=M");
+    write("@LCL");
+    write("M=D");
+
+    write("@R14");
+    write("A=M");
+    write("0;JMP");
+
+    wroteCall = true;
+}
+
+void writeCall(const char* Command[]) {
+    // Storing value of new ARG in temp variable R13
+    write("@" + atoi(Command[2]));
+    write("D=A");
+    write("@SP");
+    write("D=M-D");
+    write("@R13");
+    write("M=D");
+
+    // Pushing returnAddress on stack
+    char temp[13 + currFunctionLength];
+    snprintf(temp, 13 + currFunctionLength, "@%s$ret.%d", currFunction, callCount);
+    helperCall(temp);
+
+    // Storing the function address in temp variable R14
+    int c1 = strlen(Command[1]);
+    char temp2[c1 + 2];
+    snprintf(temp2, c1 + 2, "@%s", Command[1]);
+
+    write(temp2);
+    write("D=A");
+    write("@R14");
+    write("M=D");
+    write("@FUNCTION");
+    write("0;JMP");
+
+    if (!wroteCall)
+        commonCall();
+
+    snprintf(temp, 13 + currFunctionLength, "(%s$ret.%d)", currFunction, callCount);
+    write(temp);
+
+    ++callCount;
+}
+
+void writeFunction(const char* Command[]) {
+    currFunction = (char*) Command[1];
+    currFunctionLength = strlen(currFunction);
+
+    char temp[3 + currFunctionLength];
+    snprintf(temp, 3 + currFunctionLength, "(%s)", currFunction);
+    write(temp);
+
+    if (strcmp(Command[2], "0") != 0) {
+        const int c2 = strlen(Command[2]);
+        char temp2[c2 + 2];
+        snprintf(temp2, c2 + 2, "@%s", Command[2]);
+
+        write(temp2);                    // pushing 0 nVar times
+        write("D=A");
+
+        char temp3[15];
+        snprintf(temp3, 15, "(PushZero%d)", boolCount);
+        write(temp3);
+        write("@SP");
+        write("M=M+1");
+        write("A=M-1");
+        write("M=0");
+        write("D=D-1");
+
+        snprintf(temp3, 15, "@PushZero%d", boolCount);
+        write(temp3);
+        write("D;JGT");
+
+        ++boolCount;
+    }
+}
+
+void helperReturn(const char* addr) {
+    write("@endframe");
+    write("AM=M-1");
+    write("D=M");
+    write(addr);
+    write("M=D");
+}
+
+void commonReturn() {
+    write("(RETURN)");
+    write("@LCL");           // endframe = LCL
+    write("D=M");
+    write("@endframe");
+    write("M=D");
+    write("@5");            // returnArr = *(endframe - 5)
+    write("D=A");
+    write("@endframe");
+    write("A=M-D");
+    write("D=M");
+    write("@retAddr");
+    write("M=D");
+    write("@SP");            // *ARG = pop()
+    write("A=M-1");
+    write("D=M");
+    write("@ARG");
+    write("A=M");
+    write("M=D");
+    write("@ARG");
+    write("D=M");
+    write("@SP");
+    write("M=D+1");
+
+    helperReturn("@THAT");
+    helperReturn("@THIS");
+    helperReturn("@ARG");
+    helperReturn("@LCL");
+
+    write("@retAddr");      // goto retAddr
+    write("A=M");
+    write("0;JMP");
+
+    wroteReturn = true;
+}
+
+bool functionCommand(const char* Command[]) {
+    if (strcmp(Command[0], "call") == 0) {
+        writeCall(Command);
+    } else if (strcmp(Command[0], "function") == 0) {
+        writeFunction(Command);
+    } else if (strcmp(Command[0], "return") == 0) {
+        write("@RETURN");
+        write("0;JMP");
+
+        if (!wroteReturn)
+            commonReturn();
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
 
 void parseCommand(char* command[], char* line) {
     int count = 0;
@@ -238,6 +408,11 @@ void translate(const char* Command[]) {
         fprintf(writeFile, Command[i]);
     }
     fprintf(writeFile, "\n");
+
+    if (memoryCommand(Command) || branchingCommand(Command) || functionCommand(Command) || arthlogiCommand(Command)) {
+    } else {
+        printf("Something is not right!!\n");
+    }
 }
 
 
